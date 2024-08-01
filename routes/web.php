20a500927e8529d63bfb96b8a4e1fa981e6e564e
dharmik <dhaemiknadoda0711@gmail.com>
2024-08-01 14:25:32 +0530

    create add

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\orderDiarycontroller;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 Route::any('/',[orderDiarycontroller::class,'index'])->name('index');
 
 Route::get('/Master',[orderDiarycontroller::class,'master'])->name('master');
@@ -15,10 +14,8 @@ Route::post('/user',[orderDiarycontroller::class,'user'])->name('user.create');
 Route::post('/uom',[orderDiarycontroller::class,'uom'])->name('umo.create');
 
 
-Route::any('/Payment',[orderDiarycontroller::class,'payment'])->name('payment');
-=======
-Route::get('/',[accountingcontroller::class,'index'])->name('index');
-Route::get('/Master',[accountingcontroller::class,'master'])->name('master');
-Route::get('/Payment',[accountingcontroller::class,'payment'])->name('payment');
-Route::get('/Report',[accountingcontroller::class,'report'])->name('report');
->>>>>>> b0f9d6b2a36b3f3bd36216f9c2fdc4337ced27f0
+// Route::any('/Payment',[orderDiarycontroller::class,'payment'])->name('payment');
+// Route::get('/',[accountingcontroller::class,'index'])->name('index');
+// Route::get('/Master',[accountingcontroller::class,'master'])->name('master');
+// Route::get('/Payment',[accountingcontroller::class,'payment'])->name('payment');
+// Route::get('/Report',[accountingcontroller::class,'report'])->name('report');
